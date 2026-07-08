8cd86f68998c051570e33333eb1b813fa8c05e08
Steph Crown <emmanuelstephen024@gmail.com>
2026-07-08 22:35:57 +0100

    Implement 07.sh

--- submission/07.sh
+++ submission/07.sh
@@ -1,2 +1,6 @@
 # What is the total output value of this partially signed transaction in satoshis
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+
+pbst="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
+bitcoin-cli -regtest -rpcwallet=builderswallet decodepsbt "$pbst" | jq -r ".tx.vout[].value" | awk '{sum+=$1} END {print sum}'
