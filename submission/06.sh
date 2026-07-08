ef7251f3457c041b689ebcc13a8645ea330cbef7
Steph Crown <emmanuelstephen024@gmail.com>
2026-07-08 22:32:23 +0100

    Implement 06.sh

--- submission/06.sh
+++ submission/06.sh
@@ -1,2 +1,5 @@
 # What is the hash of this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+pbst="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
+bitcoin-cli -regtest -rpcwallet=builderswallet decodepsbt "$pbst" | jq -r ".tx.hash"
