ea462645ea235b6985bed51afc951960e2332725
Steph Crown <emmanuelstephen024@gmail.com>
2026-07-08 22:45:08 +0100

    Implement 08.sh

--- submission/08.sh
+++ submission/08.sh
@@ -1,2 +1,5 @@
 # What is the receiver's address in this partially signed transaction?
 # transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
+
+psbt="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
+bitcoin-cli -regtest -rpcwallet=builderswallet decodepsbt "$psbt" | jq -r ".tx.vout[0].scriptPubKey.address"
