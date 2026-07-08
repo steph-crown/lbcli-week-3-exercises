ed746c635cb05dde06db091ced354a6ad335aaf5
Steph Crown <emmanuelstephen024@gmail.com>
2026-07-08 21:18:49 +0100

    Implement 02.sh

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,3 @@
 # Create a native segwit address and get the public key from the address.
+ADDRESS=$(bitcoin-cli -regtest getnewaddress "" "p2sh-segwit")
+bitcoin-cli -regtest -rpcwallet=builderswallet getaddressinfo $ADDRESS | jq -r ".pubkey"
