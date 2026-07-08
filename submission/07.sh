10c28abd286ccfe7c88d2f4fdf9c251317f7b3e0
Steph Crown <emmanuelstephen024@gmail.com>
2026-07-08 22:41:11 +0100

    Update 07.sh

--- submission/07.sh
+++ submission/07.sh
@@ -3,4 +3,4 @@
 
 
 pbst="cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA"
-bitcoin-cli -regtest -rpcwallet=builderswallet decodepsbt "$pbst" | jq -r ".tx.vout[].value" | awk '{sum+=$1} END {print sum}'
+bitcoin-cli -regtest -rpcwallet=builderswallet decodepsbt "$pbst" | jq -r ".tx.vout[].value" | awk '{sum+=$1} END {printf "%.0f\n", sum*100000000}'
